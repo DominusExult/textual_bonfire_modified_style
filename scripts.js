32969ecf10b262896647d437c7dec507d2f0d85a
DominusExult <domiman+github@gmail.com>
2014-10-21 23:23:15 +0200

    got rid of the memoji thing since the built-in Haddon of Textual 5 works fine

--- scripts.js
+++ scripts.js
@@ -16,11 +16,6 @@
 	  };
 
 	  Textual.newMessagePostedToView = function(lineNumber) {
-	  	if (!replaceEmoji(lineNumber)) {
-	  		setTimeout(function() {
-	  		replaceEmoji(lineNumber);
-	  		}, 500);
-	  	}
 	    return Bonfire.message(lineNumber);
 	  };
 	  
@@ -329,82 +324,5 @@
     return Renderer;
 
   })();
-
-  replaceEmoji = function(lineNumber) {
-  	var emoji = {
-  			":)": "😊",
-  			":-)": "😊",
-  			"(:": "😊",
-  			":D": "😃",
-  			";D": "😄",
-  			";)": "😉",
-  			";-)": "😉",
-  			";P": "😜",
-  			";-P": "😜",
-  			":P": "😝",
-  			":-P": "😝",
-  			"o_o": "😳",
-  			"O_O": "😳",
-  			"o_O": "😳",
-  			"0_o": "😳",
-  			"o_0": "😳",
-  			"0_0": "😳",
-  			"O_o": "😳",
-  			":@": "😡",
-  			">.<": "😣",
-  			">_<": "😫",
-  			":(": "😞",
-  			":-(": "😞",
-  			"n_n": "😄",
-  			"u_u": "😔",
-  			"^_^": "😄",
-  			"^.^": "😊",
-  			"x.x": "😵",
-  			"x.X": "😵",
-  			"X.x": "😵",
-  			"X.X": "😵",
-  			"x_x": "😲",
-  			"x_X": "😲",
-  			"X_x": "😲",
-  			"X_X": "😲",
-  			"D:": "😧",
-  			":s": "😖",
-  			":S": "😖",
-  			"DD:": "😫",
-  			"._.'": "😰",
-  			"._.": "😞",
-  			";_;": "😢",
-  			";__;": "😢",
-  			"D;": "😰",
-  			"^_^'": "😅",
-  			"^.^": "😅",
-  			"T_T": "😭",
-  			"T.T": "😭",
-  			":|": "😐",
-  			":o": "😯",
-  			":O": "😱",
-  			":0": "😱",
-  			"-_-": "😑",
-  			":*": "😚",
-  			":**": "😘",
-  			":***": "😘",
-  			"*_*": "😍",
-			":/": "😕",
-			"😕/": "://"
-  		},
-  		line = document.querySelector("#line" + lineNumber + " .message");
-
-  	if (line) {
-  		for (var i in emoji) {
-  			while (line.innerHTML.indexOf(i) != -1) {
-  				line.innerHTML = line.innerHTML.replace(i, emoji[i]);
-  			}
-  		}
-
-  		return true;
-  	}
-
-  	return false;
-  }
   
 }).call(this);
